ccf42c2caf46bc1f10ae6fd9bdd64c3f8a8a9148
pavelkor91 <pavelkor91@gmail.com>
2017-02-08 20:20:46 +0300

    1commit

---
+++ EmployeeClient/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace EmployeeClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.employeeBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.employeeBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.employeeDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingNavigator)).BeginInit();
+            this.employeeBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataSource = typeof(EmployeeClient.ServiceReference1.Employee);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            this.bindingNavigatorAddNewItem.Click += new System.EventHandler(this.bindingNavigatorAddNewItem_Click);
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            this.bindingNavigatorDeleteItem.Click += new System.EventHandler(this.bindingNavigatorDeleteItem_Click);
+            // 
+            // employeeBindingNavigatorSaveItem
+            // 
+            this.employeeBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.employeeBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("employeeBindingNavigatorSaveItem.Image")));
+            this.employeeBindingNavigatorSaveItem.Name = "employeeBindingNavigatorSaveItem";
+            this.employeeBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
+            this.employeeBindingNavigatorSaveItem.Text = "Сохранить данные";
+            this.employeeBindingNavigatorSaveItem.Click += new System.EventHandler(this.employeeBindingNavigatorSaveItem_Click);
+            // 
+            // employeeBindingNavigator
+            // 
+            this.employeeBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.employeeBindingNavigator.CountItem = null;
+            this.employeeBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.employeeBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorAddNewItem,
+            this.toolStripSeparator2,
+            this.bindingNavigatorDeleteItem,
+            this.toolStripSeparator3,
+            this.employeeBindingNavigatorSaveItem,
+            this.toolStripSeparator1});
+            this.employeeBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.employeeBindingNavigator.MoveFirstItem = null;
+            this.employeeBindingNavigator.MoveLastItem = null;
+            this.employeeBindingNavigator.MoveNextItem = null;
+            this.employeeBindingNavigator.MovePreviousItem = null;
+            this.employeeBindingNavigator.Name = "employeeBindingNavigator";
+            this.employeeBindingNavigator.PositionItem = null;
+            this.employeeBindingNavigator.Size = new System.Drawing.Size(545, 25);
+            this.employeeBindingNavigator.TabIndex = 0;
+            this.employeeBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // employeeDataGridView
+            // 
+            this.employeeDataGridView.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.employeeDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.employeeDataGridView.Location = new System.Drawing.Point(0, 25);
+            this.employeeDataGridView.Name = "employeeDataGridView";
+            this.employeeDataGridView.Size = new System.Drawing.Size(545, 258);
+            this.employeeDataGridView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(545, 283);
+            this.Controls.Add(this.employeeDataGridView);
+            this.Controls.Add(this.employeeBindingNavigator);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сотрудники";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingNavigator)).EndInit();
+            this.employeeBindingNavigator.ResumeLayout(false);
+            this.employeeBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton employeeBindingNavigatorSaveItem;
+        private System.Windows.Forms.BindingNavigator employeeBindingNavigator;
+        private System.Windows.Forms.DataGridView employeeDataGridView;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ EmployeeClient/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EmployeeClient.ServiceReference1;
+
+namespace EmployeeClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private EmployeesEntities proxy;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             this.proxy = new EmployeesEntities
+                (new Uri("http://localhost:2671/EmployeeService.svc/"));
+             this.iniNavigator();
+             this.createColumns();
+             this.fillDataGrid();
+
+        }
+        private void createColumns()
+        {
+            this.employeeDataGridView.Columns.Add("PersonId", "Порядковый номер");
+            this.employeeDataGridView.Columns.Add("Name", "Имя");
+            this.employeeDataGridView.Columns.Add("Sename", "Фамилия");
+            this.employeeDataGridView.Columns.Add("Patronymic", "Отчество");
+            this.employeeDataGridView.Columns.Add("Date", "Дата");
+        }
+
+        private void fillDataGrid()
+        {
+            this.employeeDataGridView.Rows.Clear();
+            var list = this.proxy.Employee.ToArray();
+            foreach (var row in list)
+            {
+
+                this.employeeDataGridView.Rows.Add(row.PersonId, row.Name, 
+                    row.Sename, row.Patronymic, row.Date);
+            }
+
+        }
+        
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+
+            this.employeeDataGridView.Rows.Add();
+        }
+        private void MakeRead()
+        {
+            this.employeeDataGridView.AllowUserToAddRows = true;
+            this.employeeDataGridView.AllowUserToDeleteRows = true;
+            this.employeeDataGridView.ReadOnly = false;
+        }
+        private void iniNavigator()
+        {
+            this.bindingNavigatorDeleteItem.Enabled = true;
+            this.bindingNavigatorAddNewItem.Enabled = true;
+            this.MakeRead();
+        }
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in this.employeeDataGridView.SelectedRows)
+            {
+
+                var _employee = this.getEmployeeFromGridCell(row.Cells[0].Value);
+                if (_employee != null)
+                {
+                    this.proxy.DeleteObject(_employee);
+                }
+
+                this.employeeDataGridView.Rows.Remove(row);
+
+            }
+            this.proxy.SaveChanges();
+            this.fillDataGrid();
+        }
+
+        private void employeeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in this.employeeDataGridView.Rows)
+            {
+                if (row.Index == this.employeeDataGridView.Rows.Count - 1)
+                {
+                    continue;
+                }
+
+                var _employee = this.getEmployeeFromGridCell(row.Cells[0].Value);
+
+                if (_employee != null)
+                {
+                    _employee.Name = row.Cells[1].Value.ToString();
+                    _employee.Sename = row.Cells[2].Value.ToString();
+                    _employee.Patronymic = row.Cells[3].Value.ToString();
+                    _employee.Date = DateTime.Parse(row.Cells[4].Value.ToString());
+                    this.proxy.UpdateObject(_employee);
+                }
+                else
+                {
+                    Employee newEmployee = new Employee();
+                    newEmployee.Name = row.Cells[1].Value.ToString();
+                    newEmployee.Sename = row.Cells[2].Value.ToString();
+                    newEmployee.Patronymic = row.Cells[3].Value.ToString();
+                    newEmployee.Date = DateTime.Parse(row.Cells[4].Value.ToString());
+                    this.proxy.AddToEmployee(newEmployee);
+                }
+            }
+            this.proxy.SaveChanges();
+
+            this.fillDataGrid();
+            
+        }
+        private Employee getEmployeeFromGridCell(Object gridId)
+        {
+            int id;
+            if (gridId == null)
+                return null;
+            if (!int.TryParse(gridId.ToString(), out id))
+                return null;
+            var query = from o in this.proxy.Employee
+                        where o.PersonId == id
+                        select o;
+            var _employee = query.First();
+
+            return _employee;
+        }
+
+
+        
+    }
+}
